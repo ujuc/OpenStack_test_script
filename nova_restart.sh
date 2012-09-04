cb85fc95778c05e6dd7cfdcfe33f2f34982cf1c7
Gang Sung Jin <ujuc@ujuc.kr>
2012-09-04 16:58:40 +0900

    nova 모듈 재시작용으로 새로 생

---
+++ nova_restart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo stop libvirt-bin; sudo stop nova-network; sudo stop nova-compute; sudo stop nova-api; sudo stop nova-objectstore; sudo stop nova-scheduler; sudo stop nova-volume; sudo stop nova-consoleauth;
+
+sudo start libvirt-bin; sudo start nova-network; sudo start nova-compute; sudo start nova-api; sudo start nova-objectstore; sudo start nova-scheduler; sudo start nova-volume; sudo start nova-consoleauth;
+
