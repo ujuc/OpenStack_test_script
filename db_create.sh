a9e2529078ecde8b960f1c337e6ca284ded38d3a
Sungjin Gang <ujuc@ujuc.kr>
2012-09-27 18:01:12 +0900

    essex
    우선 태스트 해보고 master에 적용할것.

--- db_create.sh
+++ db_create.sh
@@ -4,13 +4,14 @@
 # Editer : ujuc
 # 원본: http://docs.openstack.org/essex/openstack-compute/starter/content/Creating_Databases-d1e921.html#comment-562723657
 
+HOST_IP=
 
 for app in nova glance keystone quantum swift
 do
 sudo mysql -uroot -proot00 -v -e "
 CREATE DATABASE ${app};
 CREATE USER ${app}db;
-GRANT ALL PRIVILEGES ON ${app}.* TO '${app}db'@'%';
+GRANT ALL PRIVILEGES ON ${app}.* TO '${app}db'@'${HOST_IP}';
 SET PASSWORD FOR '${app}db'@'%' = PASSWORD('${app}00');
 ";
 done
