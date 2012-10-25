7dbea9068964128077834ced54fa37f307241f20
ujuc <ujuc@ujuc.kr>
2012-10-25 11:57:01 +0900

    folsom용으로 다시 제작함.
    folsom 이나왔으니 folsom에 맞게 변경
    cinder 추가.
    endpoint 자동생성 스크립트 변경

--- db_create.sh
+++ db_create.sh
@@ -5,12 +5,12 @@
 # origin: http://docs.openstack.org/essex/openstack-compute/starter/content/Creating_Databases-d1e921.html#comment-562723657
 
 
-for app in nova glance keystone quantum swift
+for app in nova glance keystone quantum swift cinder
 do
 sudo mysql -uroot -proot00 -v -e "
 CREATE DATABASE ${app};
 CREATE USER ${app}db;
-GRANT ALL PRIVILEGES ON ${app}.* TO '${app}db'@'%';
-SET PASSWORD FOR '${app}db'@'%' = PASSWORD('${app}00');
+GRANT ALL PRIVILEGES ON ${app}.* TO '${app}db'@'localhost';
+SET PASSWORD FOR '${app}db'@'localhost' = PASSWORD('${app}00');
 ";
 done
--- nova_restart.sh
+++ nova_restart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo stop libvirt-bin; sudo stop nova-network; sudo stop nova-compute; sudo stop nova-api; sudo stop nova-objectstore; sudo stop nova-scheduler; sudo stop nova-volume; sudo stop nova-consoleauth; sudo stop nova-cert;
+sudo stop libvirt-bin; sudo start libvirt-bin;
 
-sudo start libvirt-bin; sudo start nova-network; sudo start nova-compute; sudo start nova-api; sudo start nova-objectstore; sudo start nova-scheduler; sudo start nova-volume; sudo start nova-consoleauth; sudo start nova-cert;
-
+cd /etc/init.d/; for i in $( ls nova-* ); do sudo service $i restart; done; cd ~/;
