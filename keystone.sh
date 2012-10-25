f5918f39b625163e1132315ca340500e6566df03
ujuc <ujuc@ujuc.kr>
2012-10-25 16:04:35 +0900

    keystone 작업 변경
    몇몇 부분 변경

--- keystone.sh
+++ keystone.sh
@@ -10,17 +10,13 @@
 # Endpoint setting
 # NOT used '#' add
 
-#ADMIN_PASSWORD=${ADMIN_PASSWORD:-passwd}
-SERVICE_PASSWORD=${SERVICE_PASSWORD:-$ADMIN_PASSWORD}
-SERVICE_TENANT_NAME=${SERVICE_TENANT_NAME:-service}
-
 function get_id () {
     echo `$@ | awk '/ id / { print $4 }'`
 }
 
 # Tenants
 ADMIN_TENANT=$(get_id keystone tenant-create --name admin)
-SERVICE_TENANT=$(get_id keystone tenant-create --name $SERVICE_TENANT_NAME)
+SERVICE_TENANT=$(get_id keystone tenant-create --name service)
 DEMO_TENANT=$(get_id keystone tenant-create --name demo)
 
 # Users
@@ -31,30 +27,23 @@ NOVA_USER=$(get_id keystone user-create --name nova --pass nova --email nova@foo
 GLANCE_USER=$(get_id keystone user-create --name glance --pass glance  --email glance@foo.kr)
 SWIFT_USER=$(get_id keystone user-create --name swift --pass swift --email swift@foo.kr)
 QUANTUM_USER=$(get_id keystone user-create --name quantum --pass quantum --email quantum@foo.kr)
+CINDER_USER=$(get_id keystone user-create --name cinder -pass cinder --email cinder@foo.kr)
 
 # Roles
 ADMIN_ROLE=$(get_id keystone role-create --name admin)
 MEMBER_ROLE=$(get_id keystone role-create --name Member)
 
-# Add Roles to Users in Tenants (Essex)
-keystone user-role-add --user $ADMIN_USER --role $ADMIN_ROLE --tenant_id $ADMIN_TENANT
+# Add Roles to Users in Tenants
+keystone user-role-add --user-id $ADMIN_USER --role-id $ADMIN_ROLE --tenant-id $ADMIN_TENANT
 
-keystone user-role-add --user $ADMIN_USER --role $ADMIN_ROLE --tenant_id $DEMO_TENANT
-keystone user-role-add --user $DEMO_USER --role $MEMBER_ROLE --tenant_id $DEMO_TENANT
+keystone user-role-add --user-id $ADMIN_USER --role-id $ADMIN_ROLE --tenant-id $DEMO_TENANT
+keystone user-role-add --user-id $DEMO_USER --role-id $MEMBER_ROLE --tenant-id $DEMO_TENANT
 
-keystone user-role-add --tenant_id $SERVICE_TENANT --user $NOVA_USER --role $ADMIN_ROLE
-keystone user-role-add --tenant_id $SERVICE_TENANT --user $GLANCE_USER --role $ADMIN_ROLE
-keystone user-role-add --tenant_id $SERVICE_TENANT --user $SWIFT_USER --role $ADMIN_ROLE
-keystone user-role-add --tenant_id $SERVICE_TENANT --user $QUANTUM_USER --role $ADMIN_ROLE
-
-# Sevices
-keystone service-create --name nova --type compute --description "OpenStack Compute Service"
-keystone service-create --name volume --type volume --description "OpenStack Volume Service"
-keystone service-create --name glance --type image --description "OpenStack Image Service"
-keystone service-create --name swift --type object-store --description "OpenStack Storage Service"
-keystone service-create --name keystone --type identity --description "OpenStack Identity Service"
-keystone service-create --name quantum --type network --description "OpenStack Network Service"
-keystone service-create --name ec2 --type ec2 --description "EC2 Service"
+keystone user-role-add --user-id $NOVA_USER --role-id $ADMIN_ROLE --tenant-id $SERVICE_TENANT
+keystone user-role-add --user-id $GLANCE_USER --role-id $ADMIN_ROLE --tenant-id $SERVICE_TENANT
+keystone user-role-add --user-id $SWIFT_USER --role-id $ADMIN_ROLE --tenant-id $SERVICE_TENANT
+keystone user-role-add --user-id $QUANTUM_USER --role-id $ADMIN_ROLE --tenant-id $SERVICE_TENANT
+keystone user-role-add --user-id $CINDER_USER --role-id $ADMIN_ROLE --tenant-id $SERVICE_TENANT
 
 # list view
 echo "tenant list"
@@ -63,5 +52,3 @@ echo "user list"
 keystone user-list
 echo "role-list"
 keystone role-list
-echo "service list"
-keystone service-list
